b6d2c509a75ddc125420106ff5f6708303f9d780
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-09-24 11:34:04 -0300

    adding solution to problem 27

---
+++ 27/index.js
@@ -0,0 +1,10 @@
+function removeElement(nums, val) {
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    return nums.length;
+}
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
