09c90a6b3312cc636b7752cb40b0eaadcec042ce
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-11-24 20:12:25 -0300

    adding solution to problem 108

---
+++ 108/index.ts
@@ -0,0 +1,16 @@
+function containsDuplicate(nums: number[]): boolean {
+  const obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!obj[nums[i]]) {
+      obj[nums[i]] = true;
+      continue;
+    }
+    return true;
+  }
+  return false;
+}
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
