c560b76a88c45ef91b977cbbcd2acc6db4b1ed3a
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-12-08 17:07:04 -0300

    fixing issues

--- 108/index.ts
+++ 108/index.ts
@@ -12,4 +12,17 @@
  * }
  */
 
-function sortedArrayToBST(nums: number[]): TreeNode | null {}
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+  return null;
+}
