975e38646279d25f13fe0b261bea019c9a6a5a30
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-12-22 13:05:09 -0300

    finishing problem 169

---
+++ 169/index.js
@@ -0,0 +1,20 @@
+function majorityElement(nums) {
+    var obj = {};
+    var halfLen = nums.length / 2;
+    for (var i = 0; i < nums.length; i++) {
+        if (!obj[nums[i]]) {
+            obj[nums[i]] = 1;
+            if (obj[nums[i]] >= halfLen) {
+                return nums[i];
+            }
+            continue;
+        }
+        obj[nums[i]]++;
+        if (obj[nums[i]] >= halfLen) {
+            return nums[i];
+        }
+    }
+    return 0;
+}
+console.log(majorityElement([3, 2, 3]));
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
