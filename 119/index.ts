a1792b6668add67a51ca0e3a4bb89e8d126cf884
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-12-22 12:56:22 -0300

    finishing problem 119

--- 119/index.ts
+++ 119/index.ts
@@ -1,3 +1,28 @@
 function getRow(rowIndex: number): number[] {
-  return [];
+  function generate(numRows: number): number[][] {
+    const arr: number[][] = [];
+    for (let i = 0; i < numRows; i++) {
+      if (i === 0) {
+        arr.push([1]);
+        continue;
+      }
+
+      for (let j = 0; j < i + 1; j++) {
+        if (j === 0) {
+          arr[i] = [];
+          arr[i].push(1);
+          continue;
+        } else if (j === arr[i - 1].length) {
+          arr[i].push(1);
+          continue;
+        }
+        arr[i].push(arr[i - 1][j - 1] + arr[i - 1][j]);
+      }
+    }
+    return arr;
+  }
+
+  return generate(rowIndex + 1)[rowIndex];
 }
+
+console.log(getRow(3));
