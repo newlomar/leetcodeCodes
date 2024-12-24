7d817684a71441d81af21745fc4bb6dc2955297a
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-12-24 13:55:55 -0300

    adding README to problem 228

---
+++ 228/README.md
@@ -0,0 +1,1 @@
+run with this command: `tsc -t ES2022 --lib "ES2022","DOM" index.ts`
--- 228/index.js
+++ 228/index.js
@@ -1,22 +1,22 @@
 function summaryRanges(nums) {
-    var list_array = [];
+    const list_array = [];
     while (nums.length > 0) {
         if (list_array.length === 0) {
             list_array.push(nums.splice(0, 1));
             continue;
         }
-        var last_array = list_array.at(-1);
+        const last_array = list_array.at(-1);
         if (nums[0] - last_array.at(-1) === 1) {
-            last_array === null || last_array === void 0 ? void 0 : last_array.push(nums.splice(0, 1)[0]);
+            last_array?.push(nums.splice(0, 1)[0]);
             continue;
         }
         list_array.push(nums.splice(0, 1));
     }
-    return list_array.map(function (item) {
+    return list_array.map((item) => {
         if (item.length === 1) {
             return String(item[0]);
         }
-        return "".concat(item.at(0), "->").concat(item.at(-1));
+        return `${item.at(0)}->${item.at(-1)}`;
     });
 }
 console.log(summaryRanges([0, 1, 2, 4, 5, 7]));
