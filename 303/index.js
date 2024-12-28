f66ccdb8bfc728f46fe82d2120f335fdc93b1cc9
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-12-28 10:44:39 -0300

    finishing problem 303

--- 303/index.js
+++ 303/index.js
@@ -1,22 +1,18 @@
-class NumArray {
-  nums: number[];
-
-  constructor(nums: number[]) {
-    this.nums = nums;
-  }
-
-  sumRange(left: number, right: number): number {
-    return this.nums.slice(left, right + 1).reduce((acc, val) => acc + val, 0);
-  }
-}
-
+var NumArray = /** @class */ (function () {
+    function NumArray(nums) {
+        this.nums = nums;
+    }
+    NumArray.prototype.sumRange = function (left, right) {
+        return this.nums.slice(left, right + 1).reduce(function (acc, val) { return acc + val; }, 0);
+    };
+    return NumArray;
+}());
 /**
  * Your NumArray object will be instantiated and called as such:
  * var obj = new NumArray(nums)
  * var param_1 = obj.sumRange(left,right)
  */
-
-const numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+var numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
 console.log(numArray.sumRange(0, 2)); // return (-2) + 0 + 3 = 1
 console.log(numArray.sumRange(2, 5)); // return 3 + (-5) + 2 + (-1) = -1
 console.log(numArray.sumRange(0, 5)); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
