6948e4d8ce92706f8d28babdaea8bef41e00020a
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-01-02 09:22:18 -0300

    finishing problem 414

---
+++ 414/index.js
@@ -0,0 +1,34 @@
+function thirdMax(nums) {
+    var max_1 = null;
+    var max_2 = null;
+    var max_3 = null;
+    for (var i = 0; i < nums.length; i++) {
+        if (max_1 == null || nums[i] >= max_1) {
+            if (nums[i] === max_1) {
+                continue;
+            }
+            max_3 = max_2;
+            max_2 = max_1;
+            max_1 = nums[i];
+            continue;
+        }
+        if (max_2 == null || nums[i] >= max_2) {
+            if (nums[i] === max_2) {
+                continue;
+            }
+            max_3 = max_2;
+            max_2 = nums[i];
+            continue;
+        }
+        if (max_3 == null || nums[i] >= max_3) {
+            max_3 = nums[i];
+            continue;
+        }
+    }
+    return max_3 !== null ? max_3 : max_1;
+}
+;
+console.log(thirdMax([3, 2, 1]));
+console.log(thirdMax([1, 2]));
+console.log(thirdMax([2, 2, 3, 1]));
+console.log(thirdMax([3, 3, 4, 3, 4, 3, 0, 3, 3]));
