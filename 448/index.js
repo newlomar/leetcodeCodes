ddb02db1ba81ca7be0eca54e2690401d125dd7bb
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-01-04 09:29:19 -0300

    finishing problem 448

---
+++ 448/index.js
@@ -0,0 +1,9 @@
+function findDisappearedNumbers(nums) {
+    const completeArray = [...Array(nums.length + 1).keys()];
+    for (let i = 0; i < nums.length; i++) {
+        completeArray[nums[i]] = null;
+    }
+    return completeArray.slice(1).filter((element) => element !== null);
+}
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
+console.log(findDisappearedNumbers([1, 1]));
