f6536437ae8c1a11c4b19e170cf5de7f5ec95f51
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-01-27 13:52:32 -0300

    progress?

--- 496/index.ts
+++ 496/index.ts
@@ -1,3 +1,13 @@
 function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
-    
-};
+  const nums1Map = {};
+  const finalArray = Array.from({length: nums1.length}, (_, i) => -1)
+
+  for (let i = 0; i < nums1.length; i++) {
+    nums1Map[nums1[i]] = {
+      
+    };
+  }
+  for (let i = 0; i < nums2.length; i++) {
+    if ()
+  }
+}
