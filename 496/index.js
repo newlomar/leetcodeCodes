557904e6b8464b1c584718539a80934f70f74b55
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-01-29 09:36:16 -0300

    finishing problem 496

---
+++ 496/index.js
@@ -0,0 +1,31 @@
+function nextGreaterElement(nums1, nums2) {
+    var nums1Map = {};
+    // const finalArray = Array.from({ length: nums1.length }, (_, i) => -1);
+    for (var i = 0; i < nums1.length; i++) {
+        nums1Map[nums1[i]] = {
+            value: -1,
+            available: false,
+        };
+    }
+    for (var i = 0; i < nums2.length; i++) {
+        if (nums1Map[nums2[i]]) {
+            if (!nums1Map[nums2[i]].available) {
+                nums1Map[nums2[i]].available = true;
+            }
+        }
+        for (var j = 0; j < nums1.length; j++) {
+            if (!nums1Map[nums1[j]].available) {
+                continue;
+            }
+            if (nums2[i] > nums1[j]) {
+                nums1Map[nums1[j]].value = nums2[i];
+                nums1Map[nums1[j]].available = false;
+            }
+        }
+    }
+    var returnArr = nums1.map(function (item) {
+        return nums1Map[item].value;
+    });
+    return returnArr;
+}
+console.log(nextGreaterElement([2, 4], [1, 2, 3, 4]));
--- 496/index.ts
+++ 496/index.ts
@@ -1,13 +1,35 @@
 function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
   const nums1Map = {};
-  const finalArray = Array.from({length: nums1.length}, (_, i) => -1)
 
   for (let i = 0; i < nums1.length; i++) {
     nums1Map[nums1[i]] = {
-      
+      value: -1,
+      available: false,
     };
   }
+
   for (let i = 0; i < nums2.length; i++) {
-    if ()
+    if (nums1Map[nums2[i]]) {
+      if (!nums1Map[nums2[i]].available) {
+        nums1Map[nums2[i]].available = true;
+      }
+    }
+
+    for (let j = 0; j < nums1.length; j++) {
+      if (!nums1Map[nums1[j]].available) {
+        continue;
+      }
+
+      if (nums2[i] > nums1[j]) {
+        nums1Map[nums1[j]].value = nums2[i];
+        nums1Map[nums1[j]].available = false;
+      }
+    }
   }
+  const returnArr = nums1.map((item) => {
+    return nums1Map[item].value;
+  });
+  return returnArr;
 }
+
+console.log(nextGreaterElement([2, 4], [1, 2, 3, 4]));
