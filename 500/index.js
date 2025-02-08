0bcf2b3374bcdb8234aee50a0f482621c36a613e
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-02-08 12:14:47 -0300

    finishing problem 500

---
+++ 500/index.js
@@ -0,0 +1,44 @@
+function findWords(words) {
+    const firstRow = "qwertyuiop";
+    const secondRow = "asdfghjkl";
+    const thirdRow = "zxcvbnm";
+    const result = [];
+    console.log(firstRow.includes);
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        const wordArray = word.split("");
+        console.log(wordArray);
+        const isFirstRowWord = wordArray.every((item) => {
+            if (firstRow.includes(item.toLowerCase())) {
+                return true;
+            }
+            return false;
+        });
+        if (isFirstRowWord) {
+            result.push(word);
+            continue;
+        }
+        const isSecondRowWord = wordArray.every((item) => {
+            if (secondRow.includes(item.toLowerCase())) {
+                return true;
+            }
+            return false;
+        });
+        if (isSecondRowWord) {
+            result.push(word);
+            continue;
+        }
+        const isThirdRowWord = wordArray.every((item) => {
+            if (thirdRow.includes(item.toLowerCase())) {
+                return true;
+            }
+            return false;
+        });
+        if (isThirdRowWord) {
+            result.push(word);
+            continue;
+        }
+    }
+    return result;
+}
+console.log(findWords(["Hello", "Alaska", "Dad", "Peace"]));
--- 500/index.ts
+++ 500/index.ts
@@ -1,1 +1,51 @@
-function findWords(words: string[]): string[] {}
+function findWords(words: string[]): string[] {
+  const firstRow = "qwertyuiop";
+  const secondRow = "asdfghjkl";
+  const thirdRow = "zxcvbnm";
+  const result: string[] = [];
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    const wordArray = word.split("");
+    console.log(wordArray);
+    const isFirstRowWord = wordArray.every((item) => {
+      if (firstRow.includes(item.toLowerCase())) {
+        return true;
+      }
+      return false;
+    });
+
+    if (isFirstRowWord) {
+      result.push(word);
+      continue;
+    }
+
+    const isSecondRowWord = wordArray.every((item) => {
+      if (secondRow.includes(item.toLowerCase())) {
+        return true;
+      }
+      return false;
+    });
+
+    if (isSecondRowWord) {
+      result.push(word);
+      continue;
+    }
+
+    const isThirdRowWord = wordArray.every((item) => {
+      if (thirdRow.includes(item.toLowerCase())) {
+        return true;
+      }
+      return false;
+    });
+
+    if (isThirdRowWord) {
+      result.push(word);
+      continue;
+    }
+  }
+
+  return result;
+}
+
+console.log(findWords(["Hello", "Alaska", "Dad", "Peace"]));
